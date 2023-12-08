8072da82b76893d22ceceeda60ef9d36467c60e1
Umut Arslan <umut.arslan@klosebrothers.de>
2023-12-08 12:34:11 +0100

    2023-12-08_12:34:11

--- hosts/dell/default.nix
+++ hosts/dell/default.nix
@@ -13,7 +13,7 @@
 
   boot.kernelParams = [ "intel_iommu=on" ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModprobeConfig = "options vfio-pci ids=8086:1901,10de:1f12,10de:10f9,10de:1ada,10de:1adb";
+  # boot.extraModprobeConfig = "options vfio-pci ids=8086:1901,10de:1f12,10de:10f9,10de:1ada,10de:1adb";
 
   # Enable OpenGL
   hardware.opengl = {
