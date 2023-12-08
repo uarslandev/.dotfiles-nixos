185009c274191969cda078a186c31858c67690c7
Umut Arslan <umut.arslan@klosebrothers.de>
2023-12-08 22:10:07 +0100

    2023-12-08_22:10:07

--- hosts/dell/default.nix
+++ hosts/dell/default.nix
@@ -11,10 +11,6 @@
     ];
   networking.hostName = "dell";
 
-  boot.kernelParams = [ "intel_iommu=on" "i915.force_probe=46a8" ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModprobeConfig = "options vfio-pci ids=10de:1f12,10de:10f9,10de:1ada,10de:1adb";
-  boot.blacklistedKernelModules = lib.mkForce [ "nouveau" "nvidia_drm" "nvidia_modeset" "nvidia" "nvidiafb" "nvidia-gpu" "i2c_nvidia_gpu"];
-  services.xserver.videoDrivers = lib.mkForce [ "intel" ];
+  #boot.extraModprobeConfig = "options vfio-pci ids=10de:1f12,10de:10f9,10de:1ada,10de:1adb";
 }
 
