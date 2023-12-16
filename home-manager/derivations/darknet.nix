4dbad9097b344eef852424fe3345ae00594f90a4
Umut Arslan <umut_arslan@gmx.de>
2023-12-16 23:15:53 +0100

    2023-12-16_23:15:53

--- home-manager/derivations/darknet.nix
+++ home-manager/derivations/darknet.nix
@@ -4,7 +4,10 @@ stdenv.mkDerivation rec {
   name = "darknet-${version}";
   version = "1.0.0";
   src = fetchFromGitHub {
-	url = "https://github.com/pjreddie/darknet.git";
+    owner = "pjreddie";
+    repo = "darknet";
+    rev = "f6afaabcdf85f77e7aff2ec55c020c0e297c77f9";
+    sha256 = "sha256-Bhvbc06IeA4oNz93WiPmz9TXwxz7LQ6L8HPr8UEvzvE=";
   };
 
   installPhase = ''
