0ac164d368e9020c917c0427437d7a98b5c9cf11
Umut Arslan <umut_arslan@gmx.de>
2023-12-24 17:33:06 +0100

    2023-12-24_17:33:06

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -36,7 +36,7 @@
 
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
 
