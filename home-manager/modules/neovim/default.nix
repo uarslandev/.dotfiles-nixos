c74cd34cb369ec53a7b1444f652e2a5275a504ee
Umut Arslan <umut.arslan@klosebrothers.de>
2024-01-24 13:00:58 +0100

    2024-01-24_13:00:57

--- home-manager/modules/neovim/default.nix
+++ home-manager/modules/neovim/default.nix
@@ -12,5 +12,15 @@
       set softtabstop=4
       set autoindent
     '';
+	plugins = [
+	  pkgs.vimPlugins.nvim-tree-lua
+	  pkgs.vimPlugins.nvim-colorizer-lua
+	  pkgs.vimPlugins.lsp-format-nvim
+	  pkgs.vimPlugins.formatter-nvim
+	  {
+		plugin = pkgs.vimPlugins.vim-startify;
+		config = "let g:startify_change_to_vcs_root = 0";
+      }
+	];
   };
 }
