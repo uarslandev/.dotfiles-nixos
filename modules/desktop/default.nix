5992703e8152d5aaa085fd3f2b61f78d278a32a9
Umut Arslan <umut.arslan@klosebrothers.de>
2024-01-25 13:43:10 +0100

    2024-01-25_13:43:10

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -6,7 +6,7 @@
 
 {
 	services.xserver.displayManager.lightdm.enable = true;
-	#services.xserver.desktopManager.gnome.enable = true;
+	# services.xserver.desktopManager.plasma5.enable = true;
 
    services.xserver.windowManager.i3 = {
    enable = true;
