d047150cf9be479310d6322196082f4425e7cae6
Umut Arslan <umut_arslan@gmx.de>
2024-11-24 18:21:21 +0100

    2024-11-24_18:21:21

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -3,7 +3,7 @@
 {
 	services.xserver.enable = true;
 	services.xserver.displayManager.lightdm.enable = true;
-	
+	services.xserver.desktopManager.gnome.enable = true;
 	services.xserver.windowManager.xmonad = {
 		enable = true;
 		enableContribAndExtras = true;
