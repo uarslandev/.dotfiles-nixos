3f0f71e49b0b21d40e46dad11edcde5f0c29c860
Umut Arslan <umut_arslan@gmx.de>
2024-11-27 22:52:40 +0100

    2024-11-27_22:52:40

--- configs/xmonad/xmonad.hs
+++ configs/xmonad/xmonad.hs
@@ -5,6 +5,7 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.ToggleLayouts
+import XMonad.Util.Loggers
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.SpawnOnce (spawnOnce)
 import XMonad.Layout.Spacing (spacing)
@@ -44,6 +45,7 @@ myStartupHook = do
 
 myLayout = toggleLayouts  Full $ spacing 8 $ Tall 1 (3/100) (1/2)
 
+myManageHook :: ManageHook
 myManageHook = composeAll
     [ className =? "Firefox" --> doShift "2"
     , className =? "Gimp" --> doFloat
@@ -64,3 +66,32 @@ myKeys =
     , ("M-f", sendMessage (Toggle "Full"))
     , ("M-q", spawn "xmonad --recompile; pkill xmobar; xmonad --restart")
     ]
+
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppSep             = magenta " • "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
+
