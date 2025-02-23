38906aac450e03a48570e27a01782fadc2c3d485
Umut Arslan <umut_arslan@gmx.de>
2025-02-23 23:18:27 +0100

    2025-02-23_23:18:27

--- hosts/pc/hardware-configuration.nix
+++ hosts/pc/hardware-configuration.nix
@@ -26,6 +26,11 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
+  fileSystems."/home/umut/HDD" =
+    { device = "/dev/disk/by-uuid/4b8dfe14-13cf-4daf-9e0d-e9b32377fbd6";
+      fsType = "ext4";
+    };
+
   swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -33,7 +38,9 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp42s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ztfp6m6yk5.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
