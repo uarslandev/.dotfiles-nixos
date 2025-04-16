5376ecef0e0d2945ff3a619495455edca8a27eea
Umut Arslan <umut_arslan@gmx.de>
2025-04-16 11:47:53 +0200

    2025-04-16_11:47:53

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -6,47 +6,48 @@
 
 {
   imports = [
-	./packages
+    ./packages
 #	./gaming
-	./communication
-	./development
-    ./productivity
+./communication
+./development
+./productivity
 #    ./resolve
-	./scripts
+./scripts
   ];
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.nvidia.acceptLicense = true;
   nixpkgs.config.permittedInsecurePackages = [
-                "electron-25.9.0"
-              ];
+    "electron-25.9.0"
+  ];
   programs = {
+    zsh.enable = true;
     fzf.fuzzyCompletion = true;
-	xss-lock.enable = true;
-	direnv.enable = true;
+    xss-lock.enable = true;
+    direnv.enable = true;
     thunar.enable = true;
   };
   services = {
     gvfs.enable = true;
     flatpak.enable = true;
-	printing = {
-		enable = true;
-		drivers = with pkgs; [ gutenprint hplip samsung-unified-linux-driver brlaser ];
-	};
-	avahi = {
-		enable = true;
-		nssmdns4 = true;
-		openFirewall = true;
-	};
-	power-profiles-daemon.enable = true;
-	logind = {
-	lidSwitch = "ignore";
+    printing = {
+      enable = true;
+      drivers = with pkgs; [ gutenprint hplip samsung-unified-linux-driver brlaser ];
+    };
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+    power-profiles-daemon.enable = true;
+    logind = {
+      lidSwitch = "ignore";
       extraConfig = ''
         HandlePowerKey=ignore
         HandleSuspendKey=ignore
         HandleHibernateKey=ignore
         HandleLidSwitchDocked=ignore
-    '';
+      '';
+    };
   };
-};
 }
 
