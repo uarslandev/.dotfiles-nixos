fa6964094e7a5ecf1c6f46a8b35a713da6793aa4
Umut Arslan <umut_arslan@gmx.de>
2025-04-20 00:39:44 +0200

    2025-04-20_00:39:44

--- modules/programs/scripts/convert.nix
+++ modules/programs/scripts/convert.nix
@@ -1,29 +1,17 @@
-{pkgs}:
+{ pkgs }:
 
 pkgs.writeShellScriptBin "convertVideos" ''
-  originalDir="./original"
+    originalDir="./original"
 
-  if [ ! -d "$originalDir" ]; then
-      echo "Creating original directory"
-      mkdir "$originalDir"
-  fi
+    if [ ! -d "$originalDir" ]; then
+        echo "creating original directory"
+        mkdir $originalDir
+    fi
+    for video in *.mp4; do
+        noExt="''${video%.mp4}"
+        ffmpeg -i "$video" -acodec pcm_s16le -vcodec copy "$noExt.mov"
+        mv "$video" "$originalDir"
+    done
 
-  for video in *.mp4; do
-      noExt="''${video%.mp4}"
-      echo "Converting $video to $noExt.mov"
-      ffmpeg -i "$video" -acodec pcm_s16le -vcodec copy "$noExt.mov"
-      mv "$video" "$originalDir/"
-  done
-
-  for video in *.mov; do
-      noExt="''${video%.mov}"
-      if [ ! -f "$noExt.mov" ]; then
-          echo "Converting $video to $noExt.mov"
-          ffmpeg -i "$video" -acodec pcm_s16le -vcodec copy "$noExt.mov"
-      fi
-      mv "$video" "$originalDir/"
-  done
-
-  echo "Done converting videos."
-
+    echo $noExt
 ''
