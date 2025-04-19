ff24665111e02b7bffaf13e6a22d9f38ae23fbb6
Umut Arslan <umut_arslan@gmx.de>
2025-04-19 19:09:02 +0200

    2025-04-19_19:09:02

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -62,6 +62,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    exfat
 	neovim
     wget
     neovim
