bd222f271c83b7b0ac7f2b20c79c9f9a3990f86d
--global <--global>
2025-10-21 14:08:04 +0200

    2025-10-21_14:08:04

--- shells/micromamba.nix
+++ shells/micromamba.nix
@@ -6,12 +6,13 @@ let
     runScript = "zsh";
     profile = ''
       set -e
-      eval "$(micromamba shell hook --shell=posix)"
+      eval "$(micromamba shell hook --shell posix)"
       export MAMBA_ROOT_PREFIX=${builtins.getEnv "PWD"}/.mamba
       if ! test -d $MAMBA_ROOT_PREFIX/envs/my-mamba-environment; then
           micromamba create --yes -q -n my-mamba-environment
       fi
       set +e
+      alias conda=micromamba
     '';
   };
 in
