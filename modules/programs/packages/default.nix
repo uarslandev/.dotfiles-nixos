131dc63fd46205629b70bcf5bf053e8224be3124
--global <--global>
2025-10-21 10:19:04 +0200

    2025-10-21_10:19:04

--- modules/programs/packages/default.nix
+++ modules/programs/packages/default.nix
@@ -1,18 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running `nixos-help`).
-
 { config, pkgs, ... }:
 
+let
+  oldPkgs = import (builtins.fetchTarball {
+    url = "https://channels.nixos.org/nixos-24.05/nixexprs.tar.xz";
+    sha256 = "1f8j7fh0nl4qmqlxn6lis8zf7dnckm6jri4rwmj0qm1qivhr58lv";
+  }) { };
+in
 {
-  environment.systemPackages = with pkgs; [
-	xclip
-	distrobox
-	keepassxc
-	copyq
-	imagemagick
-	rclone
-	scrot
-  ];
+  environment.systemPackages = (with pkgs; [
+    xclip
+    distrobox
+    keepassxc
+    copyq
+    imagemagick
+    rclone
+    scrot
+  ]) ++ (with oldPkgs; [ grive ]);
 }
 
