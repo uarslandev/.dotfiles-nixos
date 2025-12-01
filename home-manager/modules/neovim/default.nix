497269b78fb8d0ff0a67852ca34dd5c8539df525
Umut Arslan <umut_arslan@gmx.de>
2025-12-01 23:37:19 +0100

    2025-12-01_23:37:19

--- home-manager/modules/neovim/default.nix
+++ home-manager/modules/neovim/default.nix
@@ -21,7 +21,6 @@ in
         ${builtins.readFile ./nvim/set.lua}
         ${builtins.readFile ./nvim/plugin/treesitter.lua}
         ${builtins.readFile ./nvim/plugin/matugen.lua}
-        ${builtins.readFile ./nvim/plugin/template.lua}
         ${builtins.readFile ./nvim/plugin/telescope.lua}
         ${builtins.readFile ./nvim/plugin/lsp.lua}
         ${builtins.readFile ./nvim/plugin/lazygit.lua}
@@ -33,7 +32,7 @@ in
         ${builtins.readFile ./nvim/plugin/fugitive.lua}
         ${builtins.readFile ./nvim/plugin/flutter-tools.lua}
         ${builtins.readFile ./nvim/plugin/nvim-tree.lua}
-        ${builtins.readFile ./nvim/plugin/colors.lua}
+        ${builtins.readFile ./nvim/plugin/template.lua}
         ${builtins.readFile ./nvim/plugin/floaterm.lua}
       '';
       extraPackages = with pkgs; [
