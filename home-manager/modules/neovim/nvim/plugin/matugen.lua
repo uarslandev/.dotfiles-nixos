4bc4c441bdde8d1f870ef916ca6d898499ffad51
Umut Arslan <uarslan@techfak.uni-bielefeld.de>
2025-12-02 11:48:17 +0100

    2025-12-02_11:48:17

--- home-manager/modules/neovim/nvim/plugin/matugen.lua
+++ home-manager/modules/neovim/nvim/plugin/matugen.lua
@@ -2,13 +2,13 @@
 
 local function source_matugen()
   -- Update this with the location of your output file
-  local matugen_path = os.getenv("HOME") .. "/.config/nvim/matugen.lua"  -- dofile doesn't expand $HOME or ~
+  local matugen_path = os.getenv("HOME") .. "~/.dotfiles/home-manager/modules/neovim/nvim/plugin/template.lua"  -- dofile doesn't expand $HOME or ~
 
   local file, err = io.open(matugen_path, "r")
   -- If the matugen file does not exist (yet or at all), we must initialize a color scheme ourselves
   if err ~= nil then
     -- Some placeholder theme, this will be overwritten once matugen kicks in
-    vim.cmd('colorscheme base16-catppuccin-mocha')
+    vim.cmd('colorscheme base16-catppuccin-frappe')
 
     -- Optionally print something to the user
     vim.print("A matugen style file was not found, but that's okay! The colorscheme will dynamically change if matugen runs!")
