e713f1394e10e1c26df7d88c41068beffb179f67
Umut Arslan <umut_arslan@gmx.de>
2026-06-08 19:33:03 +0200

    2026-06-08_19:33:03

--- modules/features/development/zsh.nix
+++ modules/features/development/zsh.nix
@@ -14,20 +14,21 @@
         b = "backup";
         n = "nvim";
         wip = "git add . && git commit -m 'wip' && git push";
-              nix-search = "nix search nixpkgs";
-      nix-update = "nix flake update";
-      ta = "tmux attach -t";
-      tn = "tmux new -s";
-      tls = "tmux ls";
-      tk = "tmux kill-session -t";
-      t = "tmux";
-      ga = "git add";
-      gb = "git branch -a";
-      gc = "git commit -m";
-      gp = "git push";
-      gcd = "git commit -m $(date +'%F_%T')";
-      nixpkgs-help = "chrome /nix/store/arl0kk5jl0vjyvjj6sp4mhxjclj5d8ac-nixpkgs-manual/share/doc/nixpkgs/manual.html";
-      backup = "pushd ~/.dotfiles; ga .; gcd; gp; popd";
+        nix-search = "nix search nixpkgs";
+        nix-update = "nix flake update";
+        ta = "tmux attach -t";
+        tn = "tmux new -s";
+        tls = "tmux ls";
+        tk = "tmux kill-session -t";
+        t = "tmux";
+        ga = "git add";
+        gb = "git branch -a";
+        gc = "git commit -m";
+        gp = "git push";
+        sd = "backup && shutdown now";
+        gcd = "git commit -m $(date +'%F_%T')";
+        nixpkgs-help = "chrome /nix/store/arl0kk5jl0vjyvjj6sp4mhxjclj5d8ac-nixpkgs-manual/share/doc/nixpkgs/manual.html";
+        backup = "pushd ~/.dotfiles; ga .; gcd; gp; popd";
         u = "pushd ~/.dotfiles; sudo nixos-rebuild switch --flake .#$(hostname); popd";
       };
 
@@ -36,9 +37,9 @@
       promptInit = ''
         source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
         [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
-      '';
+        '';
     };
 
     users.defaultUserShell = pkgs.zsh;
   };
-}
+                       }
