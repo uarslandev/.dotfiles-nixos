86f70b1f4e142d7fbf8005bf218c80b42f4e173d
Umut Arslan <umut_arslan@gmx.de>
2026-06-08 19:16:40 +0200

    2026-06-08_19:16:40

--- modules/hosts/thinkpad/configuration.nix
+++ modules/hosts/thinkpad/configuration.nix
@@ -44,7 +44,7 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
