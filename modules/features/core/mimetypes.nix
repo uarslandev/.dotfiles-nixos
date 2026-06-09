15cbd8a54a7d59657a36b7c9024ff233a72e4c90
Umut Arslan <umut_arslan@gmx.de>
2026-06-09 11:28:16 +0200

    2026-06-09_11:28:16

--- modules/features/core/mimetypes.nix
+++ modules/features/core/mimetypes.nix
@@ -1,40 +1,52 @@
 { self, ... }: {
   flake.nixosModules.mimetypes = { pkgs, ... }: {
-    # System-wide packages needed for these associations to work flawlessly
     environment.systemPackages = with pkgs; [
       alacritty
       kdePackages.dolphin
       firefox
       vscode
+      kdePackages.kservice # REQUIRED: Gives Dolphin the caching tool it expects
     ];
 
-    # Configure global system mime apps and default terminal standard
+    # CRITICAL FIX FOR DOLPHIN OUTSIDE PLASMA:
+    # Tells KDE apps to look for standard menus and use the correct platform data.
+    environment.sessionVariables = {
+      XDG_MENU_PREFIX = "plasma-";
+    };
+
+    # Declaratively generate the Freedesktop application menu blueprint that Dolphin demands
+    environment.etc."xdg/menus/plasma-applications.menu".text = ''
+      <!DOCTYPE Menu PUBLIC "-//freedesktop//DTD Menu 1.0//EN"
+       "http://www.freedesktop.org/standards/menu-spec/menu-1.0.dtd">
+      <Menu>
+        <Name>Applications</Name>
+        <DefaultAppDirs/>
+        <DefaultDirDirs/>
+        <DefaultLayout inline="false" show_empty="false">
+          <Merge type="all"/>
+        </DefaultLayout>
+        <Include>
+          <All/>
+        </Include>
+      </Menu>
+    '';
+
     xdg = {
       mime = {
         enable = true;
         defaultApplications = {
-          # Directories & File Browsing
           "inode/directory" = [ "org.kde.dolphin.desktop" ];
-
-          # PDF Reader
           "application/pdf" = [ "firefox.desktop" ];
-
-          # Code / Text Editing
           "text/plain" = [ "code.desktop" ];
           "text/markdown" = [ "code.desktop" ];
           "application/json" = [ "code.desktop" ];
           "text/javascript" = [ "code.desktop" ];
-          
-          # Web / Links
           "text/html" = [ "firefox.desktop" ];
           "x-scheme-handler/http" = [ "firefox.desktop" ];
           "x-scheme-handler/https" = [ "firefox.desktop" ];
-          "x-scheme-handler/about" = [ "firefox.desktop" ];
-          "x-scheme-handler/unknown" = [ "firefox.desktop" ];
         };
       };
       
-      # Sets Alacritty as the default fallback system handler for terminal tasks
       terminal-exec = {
         enable = true;
         package = pkgs.alacritty;
