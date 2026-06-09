bbc8fc41ffbdcb05c3a06936632b18e9224f0368
Umut Arslan <umut_arslan@gmx.de>
2026-06-09 13:00:49 +0200

    2026-06-09_13:00:49

--- modules/features/default.nix
+++ modules/features/default.nix
@@ -3,17 +3,17 @@
     ./applications
     ./development
     ./system
-    ./gaming
+    ./gaming/default.nix # Explicitly import the default.nix from gaming folder
   ];
 
   flake.nixosModules.features =
     { ... }:
     {
       imports = [
-        self.nixosModules.applications
-        self.nixosModules.development
-        self.nixosModules.system
-        self.nixosModules.gaming
+        self.nixosModules.applications # This should be self.nixosModules.applications
+        self.nixosModules.development # This should be self.nixosModules.development
+        self.nixosModules.system # This should be self.nixosModules.system
+        self.nixosModules.gaming # This should be self.nixosModules.gaming
       ];
     };
 }
--- modules/features/development/default.nix
+++ modules/features/development/default.nix
@@ -1,42 +1,13 @@
 { self, inputs, ... }: {
-  flake.nixosModules.gaming =
-    { pkgs, ... }:
+  imports = [
+    ./multimedia.nix
+  ];
+
+  flake.nixosModules.applications =
+    { ... }:
     {
-      hardware.graphics = {
-        enable = true;
-        enable32Bit = true;
-      };
-
-      programs = {
-        gamemode.enable = true;
-
-        gamescope = {
-          enable = true;
-          capSysNice = true;
-        };
-
-        steam = {
-          enable = true;
-          gamescopeSession.enable = true;
-          remotePlay.openFirewall = true;
-          dedicatedServer.openFirewall = true;
-          extraCompatPackages = with pkgs; [
-            proton-ge-bin
-          ];
-        };
-      };
-
-      environment.systemPackages = with pkgs; [
-        bottles
-        goverlay
-        heroic
-        lutris
-        mangohud
-        protontricks
-        protonup-qt
-        scanmem
-        steamtinkerlaunch
-        winetricks
+      imports = [
+        self.nixosModules.multimedia # This should be self.nixosModules.multimedia
       ];
     };
 }
