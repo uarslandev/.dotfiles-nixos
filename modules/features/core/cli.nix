1c49ec0210acbdb812fe45c6c7b214db195c1215
Umut Arslan <umut_arslan@gmx.de>
2026-06-10 11:36:56 +0200

    2026-06-10_11:36:56

--- modules/features/core/cli.nix
+++ modules/features/core/cli.nix
@@ -25,5 +25,20 @@
         git
         lazygit
       ];
+
+      environment.variables.EDITOR = "nvim";
+
+      programs.bash.interactiveShellInit = ''
+        bind '"\e[1;5C": forward-word'
+        bind '"\e[1;5D": backward-word'
+        bind '"\e[1;3C": forward-word'
+        bind '"\e[1;3D": backward-word'
+        bind '"\e[3;5~": kill-word'
+        bind '"\e[3;3~": kill-word'
+        bind '"\e[H": beginning-of-line'
+        bind '"\e[F": end-of-line'
+        bind '"\C-h": backward-kill-word'
+        bind '"\e\x7f": backward-kill-word'
+      '';
     };
 }
--- modules/features/development/zsh.nix
+++ modules/features/development/zsh.nix
@@ -30,6 +30,7 @@
         nixpkgs-help = "chrome /nix/store/arl0kk5jl0vjyvjj6sp4mhxjclj5d8ac-nixpkgs-manual/share/doc/nixpkgs/manual.html";
         backup = "pushd ~/.dotfiles; ga .; gcd; gp; popd";
         u = "pushd ~/.dotfiles; sudo nixos-rebuild switch --flake .#$(hostname); popd";
+        ssh = "TERM=xterm-256color ssh";
       };
 
       histSize = 10000;
@@ -38,6 +39,29 @@
         source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
         [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
         '';
+
+      initExtra = ''
+        # Use emacs keybindings even if EDITOR is set to vi
+        bindkey -e
+
+        # Movement
+        bindkey "^[[1;5C" forward-word                # Ctrl+Right
+        bindkey "^[[1;5D" backward-word               # Ctrl+Left
+        bindkey "^[[1;3C" forward-word                # Alt+Right
+        bindkey "^[[1;3D" backward-word               # Alt+Left
+        bindkey "^[[H" beginning-of-line              # Home
+        bindkey "^[[F" end-of-line                    # End
+        bindkey "^[[3~" delete-char                   # Delete
+        bindkey "^[[3;5~" kill-word                   # Ctrl+Delete
+        bindkey "^[[3;3~" kill-word                   # Alt+Delete
+        bindkey "^[[3;2~" backward-kill-word          # Shift+Delete
+        bindkey "^H" backward-kill-word               # Ctrl+Backspace
+        bindkey "^[^?" backward-kill-word             # Alt+Backspace
+        bindkey "^[[Z" reverse-menu-complete          # Shift+Tab
+
+        # Make word navigation behave more like bash
+        WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
+      '';
     };
 
     users.defaultUserShell = pkgs.zsh;
