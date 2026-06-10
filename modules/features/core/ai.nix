76c001bcdfee006d2275e9217ebe58b1ef3fc863
Umut Arslan <umut_arslan@gmx.de>
2026-06-10 09:38:43 +0200

    2026-06-10_09:38:43

--- modules/features/core/ai.nix
+++ modules/features/core/ai.nix
@@ -6,6 +6,7 @@
       environment.systemPackages = with pkgs; [
         gemini-cli
         codex
+        claude-code
       ];
     };
 }
--- modules/features/core/cli.nix
+++ modules/features/core/cli.nix
@@ -1,26 +1,30 @@
-{ inputs, ... }: {
+{ inputs, ... }:
+{
 
-    flake.nixosModules.cli = {pkgs, ...}: {
-        environment.systemPackages = with pkgs; [
-            ripgrep
-            fd
-            gh
-            fzf
-            eza
-            bat
-            jq
-            yq
-            unzip
-            zip
-            wget
-            curl
-            tree
-            btop
-            fastfetch
-            rsync
-            just
-            git
-            lazygit
-        ];
+  flake.nixosModules.cli =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        ripgrep
+        fd
+        gh
+        fzf
+        eza
+        bat
+        jq
+        yq
+        unzip
+        zip
+        wget
+        curl
+        tree
+        btop
+        fastfetch
+        rsync
+        just
+        git
+        lazygit
+      ];
     };
 }
+
