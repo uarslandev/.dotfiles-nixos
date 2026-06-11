8604bbf8f3ff5f7bf90e42624f4bd1c11d7bce0b
Umut Arslan <umutarslan.dev@gmail.com>
2026-06-11 13:21:17 +0200

    2026-06-11_13:21:17

--- modules/features/core/ai.nix
+++ modules/features/core/ai.nix
@@ -1,26 +1,30 @@
-{ self, inputs, ... }: {
-  perSystem = { pkgs, ... }: 
-  let
-    pkgsUnfree = import inputs.nixpkgs {
-      inherit (pkgs.stdenv.hostPlatform) system;
-      config.allowUnfree = true;
+{ self, inputs, ... }:
+{
+  perSystem =
+    { pkgs, ... }:
+    let
+      pkgsUnfree = import inputs.nixpkgs {
+        inherit (pkgs.stdenv.hostPlatform) system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      packages.ai = inputs.wrapper-modules.wrappers.claude-code.wrap {
+        pkgs = pkgsUnfree;
+
+        # Bundling other AI tools together
+        runtimePkgs = [
+          pkgs.gemini-cli
+          pkgs.codex-acp
+        ];
+      };
     };
-  in
-  {
-    packages.ai = inputs.wrapper-modules.wrappers.claude-code.wrap {
-      pkgs = pkgsUnfree;
-      
-      # Bundling other AI tools together
-      runtimePkgs = [
-        pkgs.gemini-cli
-        pkgs.codex
+
+  flake.nixosModules.ai =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = [
+        self.packages.${pkgs.stdenv.hostPlatform.system}.ai
       ];
     };
-  };
-
-  flake.nixosModules.ai = { pkgs, ... }: {
-    environment.systemPackages = [
-      self.packages.${pkgs.stdenv.hostPlatform.system}.ai
-    ];
-  };
 }
