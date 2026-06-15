598c65bf557c9df86e611c0c00fc7da4d50d95e6
Umut Arslan <umut_arslan@gmx.de>
2026-06-15 16:12:01 +0200

    2026-06-15_16:12:01

--- modules/features/development/zsh.nix
+++ modules/features/development/zsh.nix
@@ -4,10 +4,6 @@
     { pkgs, self', ... }:
     {
       packages.tmux-sessionizer = pkgs.writeShellScriptBin "tmux-sessionizer" ''
-        if [[ -z "$TMUX" ]]; then
-            exec ${pkgs.tmux}/bin/tmux new-session -A -s main "$0" "$@"
-        fi
-
         RECENT_FILE="$HOME/.local/share/tmux-recent"
         mkdir -p "$(dirname "$RECENT_FILE")"
         touch "$RECENT_FILE"
@@ -59,19 +55,25 @@
         echo "$selected" | cat - "$RECENT_FILE" | awk '!seen[$0]++' > "$RECENT_FILE.tmp" && mv "$RECENT_FILE.tmp" "$RECENT_FILE"
 
         selected_name=$(basename "$selected" | tr . _)
+        tmux_running=$(pgrep tmux)
+
+        if [[ -z "$TMUX" ]] && [[ -z "$tmux_running" ]]; then
+            ${pkgs.tmux}/bin/tmux new-session -s "$selected_name" -c "$selected"
+            exit 0
+        fi
 
         if ! ${pkgs.tmux}/bin/tmux has-session -t "$selected_name" 2>/dev/null; then
             ${pkgs.tmux}/bin/tmux new-session -ds "$selected_name" -c "$selected"
         fi
 
-        ${pkgs.tmux}/bin/tmux switch-client -t "$selected_name"
+        if [[ -z "$TMUX" ]]; then
+            ${pkgs.tmux}/bin/tmux attach-session -t "$selected_name"
+        else
+            ${pkgs.tmux}/bin/tmux switch-client -t "$selected_name"
+        fi
       '';
 
       packages.tmux-ssh-sessionizer = pkgs.writeShellScriptBin "tmux-ssh-sessionizer" ''
-        if [[ -z "$TMUX" ]]; then
-            exec ${pkgs.tmux}/bin/tmux new-session -A -s main "$0" "$@"
-        fi
-
         RECENT_FILE="$HOME/.local/share/tmux-ssh-recent"
         mkdir -p "$(dirname "$RECENT_FILE")"
         touch "$RECENT_FILE"
@@ -173,12 +175,22 @@
 
         # Use system ssh command with explicit config file (stored as array to avoid shell quoting issues)
         ssh_cmd=(ssh -F "$HOME/.ssh/config" -t "$selected_host" "cd '$selected_dir' 2>/dev/null || cd ~; exec \$SHELL -l")
+        tmux_running=$(pgrep tmux)
+
+        if [[ -z "$TMUX" ]] && [[ -z "$tmux_running" ]]; then
+            ${pkgs.tmux}/bin/tmux new-session -s "$session_name" "''${ssh_cmd[@]}"
+            exit 0
+        fi
 
         if ! ${pkgs.tmux}/bin/tmux has-session -t "$session_name" 2>/dev/null; then
             ${pkgs.tmux}/bin/tmux new-session -ds "$session_name" "''${ssh_cmd[@]}"
         fi
 
-        ${pkgs.tmux}/bin/tmux switch-client -t "$session_name"
+        if [[ -z "$TMUX" ]]; then
+            ${pkgs.tmux}/bin/tmux attach-session -t "$session_name"
+        else
+            ${pkgs.tmux}/bin/tmux switch-client -t "$session_name"
+        fi
       '';
 
       # Portable Zsh package for use with 'nix run' on other systems
