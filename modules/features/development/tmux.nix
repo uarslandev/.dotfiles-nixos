34673a453eb78199003bfa9f263bc3c1621a41c1
Umut Arslan <umut_arslan@gmx.de>
2026-06-18 10:03:04 +0200

    13th - thinkpad

--- modules/features/development/tmux.nix
+++ modules/features/development/tmux.nix
@@ -26,9 +26,23 @@
           bind-key t new-window -c "#{pane_current_path}"
           bind-key c new-window -c "#{pane_current_path}"
 
-          # Delete current tab (window) with prefix + k or prefix + X
-          bind-key k kill-window
+          # Delete current tab (window) with prefix + X
           bind-key X kill-window
+
+          # Vim-style pane selection (prefix + hjkl)
+          bind-key h select-pane -L
+          bind-key j select-pane -D
+          bind-key k select-pane -U
+          bind-key l select-pane -R
+
+          # Increase repeat timeout for repeatable bindings (e.g. pane resizing) to 1.5 seconds
+          set -g repeat-time 1500
+
+          # Vim-style pane resizing (prefix + Alt + hjkl, repeatable)
+          bind-key -r M-h resize-pane -L 2
+          bind-key -r M-j resize-pane -D 2
+          bind-key -r M-k resize-pane -U 2
+          bind-key -r M-l resize-pane -R 2
 
           # Send current pane to a new tab (window) with prefix + b
           bind-key b break-pane
@@ -39,6 +53,10 @@
 
           # Sessionizer: run tmux-sessionizer inside a new tmux window
           bind-key -r C-f run-shell "tmux neww tmux-sessionizer"
+
+          # F12 toggles local tmux keybindings off/on for nested/remote sessions
+          bind -n F12 set prefix None \; set key-table off \; set status-left " #[fg=colour203]● [REMOTE] " \; set status-style bg=colour236,fg=colour245 \; refresh-client -S
+          bind -T off F12 set -u prefix \; set -u key-table \; set -u status-left \; set -u status-style \; refresh-client -S
         '';
       };
     };
