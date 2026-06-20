1d1d2394ef496897076aa955f65a6a59ee614fb3
Umut Arslan <umut_arslan@gmx.de>
2026-06-20 18:37:56 +0200

    22nd - thinkpad

--- modules/features/core/default.nix
+++ modules/features/core/default.nix
@@ -17,6 +17,7 @@
 
       environment.systemPackages = with pkgs; [
         anki-bin
+        google-chrome
         kdePackages.kdeconnect-kde
         sanoid
         discord
--- modules/hosts/thinkpad/configuration.nix
+++ modules/hosts/thinkpad/configuration.nix
@@ -57,6 +57,29 @@
       # Enable the X11 windowing system.
       services.xserver.enable = true;
 
+      # Bluetooht
+      hardware.bluetooth = {
+        enable = true;
+        powerOnBoot = true;
+        settings = {
+          General = {
+            # Shows battery charge of connected devices on supported
+            # Bluetooth adapters. Defaults to 'false'.
+            Experimental = true;
+            # When enabled other devices can connect faster to us, however
+            # the tradeoff is increased power consumption. Defaults to
+            # 'false'.
+            FastConnectable = true;
+          };
+          Policy = {
+            # Enable all controllers when they are found. This includes
+            # adapters present on start as well as adapters that are plugged
+            # in later on. Defaults to 'true'.
+            AutoEnable = true;
+          };
+        };
+      };
+
       # Enable the GNOME Desktop Environment.
 
       # Configure keymap in X11
